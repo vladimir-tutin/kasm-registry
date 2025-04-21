06f8f64c3150ed4b03f946d9ef5672bfd258048e
vladimir-tutin <patrick.d.coy@gmail.com>
2025-04-21 11:02:00 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Vladimir Tutins Registry',
+    description: 'My stuff.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://vladimir-tutin.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
